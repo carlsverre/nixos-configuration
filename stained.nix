8d0614bedd1bebb2c1da28743623285d08fa845b
Carl Sverre <accounts@carlsverre.com>
2016-08-06 15:09:51 -0700

    remove audit from syslog and add chromecast support

--- stained.nix
+++ stained.nix
@@ -5,6 +5,11 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.device = "/dev/sda";
 
+  # Note: in addition to disabling the audit service, nixos also requires
+  # disabling audit at boot time if you want to remove all syscall overhead
+  security.audit.enable = false;
+  boot.kernelParams = [ "audit=0" ];
+
   boot.initrd.luks.devices = [
     {
       name = "root";
@@ -14,6 +19,11 @@
   ];
 
   networking.hostName = "stained";
+
+  networking.firewall = {
+    # chromecast support
+    extraCommands = "iptables -I INPUT -p udp -m udp --dport 32768:61000 -j ACCEPT";
+  };
 
   hardware = {
     trackpoint = {
