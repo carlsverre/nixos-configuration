77ec4c47c85b2f87fd53c0443cd3f847376f5387
Carl Sverre <accounts@carlsverre.com>
2016-08-06 21:30:07 -0700

    more correct

--- software.nix
+++ software.nix
@@ -1,30 +1,33 @@
 { config, pkgs, ... }:
 
-let
-    chromium = pkgs.chromium.override {
-      enablePepperFlash = true;
-      pulseSupport = true;
+{
+  nixpkgs.config = {
+    allowUnfree = true;
+
+    chromium = {
+      enablePepperFlash = true; # Chromium removed support for Mozilla (NPAPI) plugins so Adobe Flash no longer works
+      enablePepperPDF = true;
       hiDPISupport = true;
     };
-in
-  {
-    programs.zsh.enable = true;
-    programs.light.enable = true;
+  };
 
-    environment.systemPackages = with pkgs; [
-      wget
-      rxvt_unicode-with-plugins
-      dmenu
-      i3blocks-gaps
-      git
-      htop
-      atop
-      xorg.xf86inputsynaptics
-      wpa_supplicant_gui
-      unzip
-      acpi
-      xclip
-      compton
-      chromium
-    ];
-  }
+  programs.zsh.enable = true;
+  programs.light.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    wget
+    rxvt_unicode-with-plugins
+    dmenu
+    i3blocks-gaps
+    git
+    htop
+    atop
+    xorg.xf86inputsynaptics
+    wpa_supplicant_gui
+    unzip
+    acpi
+    xclip
+    compton
+    chromium
+  ];
+}
