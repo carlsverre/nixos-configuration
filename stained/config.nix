e909b795f4ea5b9f0afb2264a60a7ff1e1b2c1bc
Carl Sverre <accounts@carlsverre.com>
2018-02-14 15:35:10 -0800

    working kerberos config

--- stained/config.nix
+++ stained/config.nix
@@ -2,6 +2,8 @@
 
 {
   environment.etc."opt/chrome/policies/managed/memcompute.json".text = builtins.toJSON {
-    AuthServerWhitelist = "*.ops.memcompute.com,*.cloud.memcompute.com";
+    AuthServerWhitelist = "*.ops.memcompute.com";
+    GSSAPILibraryName = "${pkgs.libheimdal}/lib/libgssapi.so";
+    EnableCommonNameFallbackForLocalAnchors = true;
   };
 }
