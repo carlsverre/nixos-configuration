b94aabbccc658d9e43c4ea60ed89ca4b4c8cbe94
Carl Sverre <accounts@carlsverre.com>
2018-02-14 16:09:21 -0800

    setting up krb5

--- stained/config.nix
+++ stained/config.nix
@@ -6,4 +6,24 @@
     GSSAPILibraryName = "${pkgs.libheimdal}/lib/libgssapi.so";
     EnableCommonNameFallbackForLocalAnchors = true;
   };
+
+  krb5 = {
+    enable = true;
+    kerberos = pkgs.heimdalFull;
+
+    libdefaults = {
+      default_realm = "OPS.MEMCOMPUTE.COM";
+      dns_lookup_realm = true;
+      dns_lookup_kdc = true;
+      rdns = false;
+      ticket_lifetime = "24h";
+      forwardable = "yes";
+      udp_preference_limit = 0;
+    };
+
+    domain_realm = {
+      "ops.memcompute.com" = "OPS.MEMCOMPUTE.COM";
+      ".ops.memcompute.com" = "OPS.MEMCOMPUTE.COM";
+    };
+  };
 }
