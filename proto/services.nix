09d46211bcb4d5b61844a2b5b1da95cf283057ba
Carl Sverre <accounts@carlsverre.com>
2019-01-18 07:07:54 +0000

    functions

--- proto/services.nix
+++ proto/services.nix
@@ -1,5 +1,28 @@
 { config, pkgs, ... }:
 
+let
+  redirect = (target: {
+    globalRedirect = target;
+    enableACME = true;
+    forceSSL = true;
+  });
+
+  vhost = (rootDir: {
+    forceSSL = true;
+    enableACME = true;
+
+    locations."/robots.txt" = {
+      extraConfig = ''
+        add_header Content-Type text/plain;
+        return 200 "User-agent: *\nDisallow:\n";
+      '';
+    };
+
+    locations."/" = {
+      root = rootDir;
+    };
+  });
+in
 {
   virtualisation = {
     docker = {
@@ -39,80 +62,22 @@
       '';
 
       virtualHosts = {
-        "proto.sh" = {
-          globalRedirect = "www.proto.sh";
-          enableACME = true;
-          forceSSL = true;
-        };
+        "proto.sh" = (redirect "www.proto.sh");
+        "www.proto.sh" = (vhost "/srv/sh/proto/www");
 
-        "www.proto.sh" = {
-          forceSSL = true;
-          enableACME = true;
+        "carlsverre.com" = (redirect "www.carlsverre.com");
+        "www.carlsverre.com" = (vhost "/srv/com/carlsverre/www");
 
-          locations."/" = {
-            root = "/srv/sh/proto/www";
-          };
-        };
+        "resumesculptor.com" = (redirect "www.resumesculptor.com");
+        "www.resumesculptor.com" = (vhost "/srv/com/resumesculptor/www");
 
-        "carlsverre.com" = {
-          globalRedirect = "www.carlsverre.com";
-          enableACME = true;
-          forceSSL = true;
-        };
+        # c83.me is primary
+        "www.c83.me" = (redirect "c83.me");
+        "c83.me" = (vhost "/srv/me/c83/@");
 
-        "www.carlsverre.com" = {
-          forceSSL = true;
-          enableACME = true;
-
-          locations."/" = {
-            root = "/srv/com/carlsverre/www";
-          };
-        };
-
-        "resumesculptor.com" = {
-          globalRedirect = "www.resumesculptor.com";
-          enableACME = true;
-          forceSSL = true;
-        };
-
-        "www.resumesculptor.com" = {
-          forceSSL = true;
-          enableACME = true;
-
-          locations."/" = {
-            root = "/srv/com/resumesculptor/www";
-          };
-        };
-
-        "www.c83.me" = {
-          globalRedirect = "c83.me";
-          enableACME = true;
-          forceSSL = true;
-        };
-
-        "c83.me" = {
-          forceSSL = true;
-          enableACME = true;
-
-          locations."/" = {
-            root = "/srv/me/c83/@";
-          };
-        };
-
-        "www.memory.sh" = {
-          globalRedirect = "memory.sh";
-          enableACME = true;
-          forceSSL = true;
-        };
-
-        "memory.sh" = {
-          forceSSL = true;
-          enableACME = true;
-
-          locations."/" = {
-            root = "/srv/sh/memory/@";
-          };
-        };
+        # memory.sh is primary
+        "www.memory.sh" = (redirect "memory.sh");
+        "memory.sh" = (vhost "/srv/sh/memory/@");
       };
     };
   };
